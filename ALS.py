371f9293df4c841c72b7803da8383bb47f7a3b4e
anantchowdhary <chowdhary.12@osu.edu>
2017-04-24 21:35:16 -0400

    Reverted to previous data set (ml-100k)

--- ALS.py
+++ ALS.py
@@ -11,6 +11,35 @@ def get_mse(pred, actual):
     actual = actual[actual.nonzero()].flatten()
     return mean_squared_error(pred, actual)
 
+''' Number of latent factors'''
+
+
+
+
+cols = ['user_id', 'movie_id', 'rating', 'timestamp']
+df = pd.read_csv('u.data', sep='\t', names=cols)
+del df['timestamp']
+#del df['time']
+
+#print df
+
+user_list = df['user_id'].drop_duplicates().tolist()
+user_list.sort()
+
+movie_list = df['movie_id'].drop_duplicates().tolist()
+movie_list.sort()
+
+'''Sorted user and movie list. And replace the user_id or movie_id by the index of the user/movie in 
+the user/movie list'''
+
+'''for row in range(len(df)):
+    df.set_value(row, 'user_ID', int(user_list.index(df.iloc[row]['user_id'])))
+    df.set_value(row, 'movie_ID', int(movie_list.index(df.iloc[row]['movie_id'])))'''
+
+num_users = len(user_list)
+num_movies = len(movie_list)
+
+
 def train_test_split(ratings):
     test = np.zeros(ratings.shape)
     train = ratings.copy()
@@ -24,69 +53,77 @@ def train_test_split(ratings):
 
     return train, test
 
+
+'''Creating a rating matrix'''
+
+ratings = np.zeros((num_users, num_movies))
+#print df.iloc[1]['user_id']
+
+#print user_list
+#print movie_list
+#print df
+for row in range(len(df)):
+
+    #print df.iloc[row]['user_id']
+    ratings[int(df.iloc[row]['user_id']-1), int(df.iloc[row]['movie_id'])-1] = df.iloc[row]['rating']
+
+
 def predict(u, i):
     global user_matrix
     global movie_matrix
+
     return user_matrix[u, :].dot(movie_matrix[i, :].T)
 
+##The Latent Vector for the User matrix
+
+
 def als_user(movie_mat, user_mat, ratings):
+
     global reg_user
     global num_users
 
     YTY = movie_mat.T.dot(movie_mat)
+
     const_user = np.eye(YTY.shape[0]) * reg_user
+
     for u in range(num_users):
+
         user_mat[u, :] = ratings[u, :].dot(movie_mat).dot(inv(YTY + const_user))
 
     return user_mat
 
 
 def als_movie(movie_mat, user_mat, ratings):
+
     global reg_movies
+
     global num_movies
 
     XTX = user_mat.T.dot(user_mat)
+
     const_movies = np.eye(XTX.shape[0]) * reg_movies
+
     for i in range(num_movies):
+
         movie_mat[i, :] = ratings[:, i].T.dot(user_mat).dot(inv(XTX + const_movies))
 
     return movie_mat
 
-''' Number of latent factors'''
 
-reg_user = 0.1
-reg_movies = 0.1
-
-#df = pd.read_csv('u.data', sep='\t', names=cols)
-df = pd.read_csv('ratings_processed.csv')
-#del df['time']
-
-user_list = df['user_id'].drop_duplicates().tolist()
-user_list.sort()
-
-movie_list = df['movie_id'].drop_duplicates().tolist()
-movie_list.sort()
-
-num_users = len(user_list)
-num_movies = len(movie_list)
-
-'''Creating a rating matrix'''
-ratings = np.zeros((num_users, num_movies))
-for index, row in df.iterrows():
-    ratings[int(row['user_id']), int(row['movie_id'])] = row['rating']
-
-itern_list = [ 100]
-k_list = [ 30]
+itern_list = [ 50]
+k_list = [ 40,50,60]
 #lambda_list = np.logspace(-3, 2.0, num=2)
 lambda_list = [0.1]
 
 data = []
+#print df
+
 for lambda_ in lambda_list:
     #row = []
     reg_user = lambda_
     reg_movies = lambda_
-    print " --------- lambda = " + str(lambda_) + "-------------"
     train, test = train_test_split(ratings=ratings)
+    print " --------- lambda = " + str(lambda_) + "-------------"
     #row.append(lambda_)
     for k in k_list:
         print "k = " + str(k)
@@ -97,7 +134,9 @@ for lambda_ in lambda_list:
             user_matrix = np.random.random((num_users, k))
             movie_matrix = np.random.random((num_movies, k))
             for j in range(iter_cur):
+
                 movie_matrix = als_movie(movie_mat=movie_matrix, user_mat=user_matrix, ratings=train)
+
                 user_matrix = als_user(movie_mat=movie_matrix, user_mat=user_matrix, ratings=train)
 
             predictions = np.zeros((num_users, num_movies))
@@ -119,4 +158,4 @@ for lambda_ in lambda_list:
             data.append(test_ms)
             print "Training MSE: "+str(get_mse(predictions, train)) + " Test MSE: " + str(test_ms)
 
-print min(data)
+
